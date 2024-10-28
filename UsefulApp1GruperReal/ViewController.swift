ce6d882a1400718842959e9fa468773987c012f6
Michael Gruper <sarge43210@gmail.com>
2024-10-28 08:32:42 -0500

    Initial Commit

---
+++ UsefulApp1GruperReal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsefulApp1GruperReal
+//
+//  Created by MICHAEL GRUPER on 10/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UsefulApp1GruperRealTests/UsefulApp1GruperRealTests.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulApp1GruperRealTests.swift
+//  UsefulApp1GruperRealTests
+//
+//  Created by MICHAEL GRUPER on 10/28/24.
+//
+
+import Testing
+@testable import UsefulApp1GruperReal
+
+struct UsefulApp1GruperRealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
